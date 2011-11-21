6108f47e7fe324df1ca62d5f65e54e78beecd8df
jhernst <jhernst@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-21 15:17:35 +0000

    Initial commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jhe_adventcalender/trunk@54532 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "jhe_adventcalender".
+#
+# Auto generated 21-11-2011 10:25
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Advent calender',
+	'description' => 'Provides a customizable advent calender',
+	'category' => 'plugin',
+	'author' => 'Jari-Hermann Ernst, M.A.',
+	'author_email' => 'jari-hermann.ernst@bad-gmbh.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"de36";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"035d";s:14:"ext_tables.php";s:4:"ec8f";s:13:"locallang.xml";s:4:"27e4";s:16:"locallang_db.xml";s:4:"7b6d";s:19:"doc/wizard_form.dat";s:4:"88d5";s:20:"doc/wizard_form.html";s:4:"ed68";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:38:"pi1/class.tx_jheadventcalender_pi1.php";s:4:"f37e";s:46:"pi1/class.tx_jheadventcalender_pi1_wizicon.php";s:4:"24a6";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"6436";}',
+);
+
+?>
---
+++ pi1/class.tx_jheadventcalender_pi1.php
@@ -0,0 +1,128 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Jari-Hermann Ernst, M.A. <jari-hermann.ernst@bad-gmbh.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+
+/**
+ * Plugin 'Advent calender' for the 'jhe_adventcalender' extension.
+ *
+ * @author	Jari-Hermann Ernst, M.A. <jari-hermann.ernst@bad-gmbh.de>
+ * @package	TYPO3
+ * @subpackage	tx_jheadventcalender
+ */
+class tx_jheadventcalender_pi1 extends tslib_pibase {
+	var $prefixId      = 'tx_jheadventcalender_pi1';		// Same as class name
+	var $scriptRelPath = 'pi1/class.tx_jheadventcalender_pi1.php';	// Path to this script relative to the extension dir.
+	var $extKey        = 'jhe_adventcalender';	// The extension key.
+	
+	/**
+	 * The main method of the PlugIn
+	 *
+	 * @param	string		$content: The PlugIn content
+	 * @param	array		$conf: The PlugIn configuration
+	 * @return	The content that is displayed on the website
+	 */
+	function main($content, $conf) {
+		$this->conf = $conf;
+		$this->pi_setPiVarDefaults();
+		$this->pi_loadLL();
+		$this->pi_USER_INT_obj = 1;	// Configuring so caching is not expected. This value means that no cHash params are ever set. We do this, because it's a USER_INT object!
+	
+		//get flexform data
+		$this->pi_initPIFlexForm();
+//t3lib_div::debug($this->cObj->data['pi_flexform']);
+		$this->conf['image'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],'image', 'sDEF');
+		$this->conf['imageWidth'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],'imageWidth', 'sDEF');
+		$this->conf['imageHeight'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],'imageHeight', 'sDEF');
+		$this->conf['altText'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],'altText', 'sDEF');
+		
+		$this->conf['usemap'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],'usemap', 's_imagemap');
+		$this->conf['imageMap'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],'imageMap', 's_imagemap');
+		
+		for($i=1; $i< 25; $i++){
+			$wicket = 'wicket' . $i;
+			$this->conf['wicket'][$i] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],$wicket, 's_pids');
+		}
+		
+		$this->conf['useajax'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],'useajax', 's_additional');
+		
+		//rebuild image-map
+		$imageMapArr = t3lib_div::xml2tree($this->conf['imageMap']);
+		$imageMapAreaArr = $imageMapArr['map']['0']['ch']['area'];
+		
+		$map = '<map name="' . $this->conf['usemap'] . '" id="' . $this->conf['usemap'] . '">';
+		$i=1;
+		foreach($imageMapAreaArr as $area){
+			$areaData = $area['attrs'];
+			//create typolinks for area href
+			//include_once(PATH_site.'typo3/sysext/cms/tslib/class.tslib_content.php');
+			$cObj = t3lib_div::makeInstance('tslib_cObj');
+			$hrefTarget = $cObj->typoLink_URL(array('parameter' => $this->conf['wicket'][$i]));
+
+			if($hrefTarget){
+				if($this->conf['useajax']){
+					$map .= '<area shape="' . $areaData['shape'] . '" coords="' . $areaData['coords'] . ' " onClick="alert(\'AJAX-Aufruf gestartet!\');document.getElementById(\'modal\').style.display = \'block\';" href="#" alt="' . $areaData['alt'] . '" />';
+				} else {
+					$map .= '<area shape="' . $areaData['shape'] . '" coords="' . $areaData['coords'] . ' " href="' . $hrefTarget . '" alt="' . $areaData['alt'] . '" />';
+				}
+			}
+			$i++;
+		}
+		$map .= '</map>';
+		
+		//ajax functionality
+		if($this->conf['useajax']){
+			$ajaxPanel = '
+				<div id="modal" style="display: none;position: absolute; top: 0; left: 0; z-index: 24999;width: 100%;height: 100%;background-color: #000;opacity: .5;">
+					<div id="ajaxpanel" onClick="alert(\'Ich gehe jetzt zu!\');document.getElementById(\'modal\').style.display = \'none\';" style="display: block;position: relative; width: 400px; height: 300px;margin: auto; z-index: 25000;background-color: #fff; opacity: 1;border: 3px solid green;">
+						PLATZHALTER FÜR AJAX-AUSGABE
+					</div>
+				</div>';
+		} else {
+			$ajaxPanel = '';
+		}
+		
+		$content='
+			<img src="' . $this->conf['image'] . '" alt="' . $this->conf['altText'] . '" width="' . $this->conf['imageWidth'] . '" height="' . $this->conf['imageHeight'] . '" border="0" usemap="#' . $this->conf['usemap'] . '" title="' . $this->conf['altText'] . '" />
+			' . $map . '
+			' . $ajaxPanel . '
+		';
+	
+		return $this->pi_wrapInBaseClass($content);
+	}
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/jhe_adventcalender/pi1/class.tx_jheadventcalender_pi1.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/jhe_adventcalender/pi1/class.tx_jheadventcalender_pi1.php']);
+}
+
+?>
