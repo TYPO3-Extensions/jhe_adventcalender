d01515fe3ad8fa1fef893a742f0b7fb1e7933af9
jhernst <jhernst@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-27 13:56:18 +0000

    * changed the state of the extension to obsolete
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jhe_adventcalender/trunk@81745 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "jhe_adventcalender".
  *
- * Auto generated 17-06-2013 10:55
+ * Auto generated 27-11-2013 13:54
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'obsolete',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'B·A·D Gesundheitsvorsorge und Sicherheitstechnik GmbH',
-	'version' => '0.3.1',
+	'version' => '0.3.2',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-4.7.99',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"2def";s:12:"ext_icon.gif";s:4:"2397";s:17:"ext_localconf.php";s:4:"ce61";s:14:"ext_tables.php";s:4:"f571";s:13:"locallang.xml";s:4:"261a";s:16:"locallang_db.xml";s:4:"a6a8";s:10:"README.txt";s:4:"ee2d";s:34:"ajax/class.ajax_adventcalender.php";s:4:"7a81";s:14:"doc/manual.sxw";s:4:"cda4";s:19:"doc/wizard_form.dat";s:4:"88d5";s:20:"doc/wizard_form.html";s:4:"ed68";s:14:"pi1/ce_wiz.gif";s:4:"cf99";s:38:"pi1/class.tx_jheadventcalender_pi1.php";s:4:"bbfc";s:46:"pi1/class.tx_jheadventcalender_pi1_wizicon.php";s:4:"8bee";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"4c0b";s:17:"pi1/locallang.xml";s:4:"0b81";s:16:"res/css/ajax.css";s:4:"c195";s:23:"res/img/ajax-loader.gif";s:4:"817b";s:21:"res/img/bg_dialog.gif";s:4:"2841";s:20:"res/img/bt_close.gif";s:4:"e595";s:33:"res/js/jquery/jquery-1.5.1.min.js";s:4:"5238";s:30:"res/js/jquery/jquery.cookie.js";s:4:"1252";s:26:"res/js/snow/jquery.snow.js";s:4:"ff42";s:30:"res/js/snow/jquery.snow.min.js";s:4:"4721";s:28:"res/js/snowstorm/license.txt";s:4:"ea96";s:33:"res/js/snowstorm/snowstorm-min.js";s:4:"244c";}',
+	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"8884";s:12:"ext_icon.gif";s:4:"2397";s:17:"ext_localconf.php";s:4:"ce61";s:14:"ext_tables.php";s:4:"f571";s:13:"locallang.xml";s:4:"261a";s:16:"locallang_db.xml";s:4:"a6a8";s:10:"README.txt";s:4:"ee2d";s:34:"ajax/class.ajax_adventcalender.php";s:4:"7a81";s:14:"doc/manual.sxw";s:4:"cda4";s:19:"doc/wizard_form.dat";s:4:"88d5";s:20:"doc/wizard_form.html";s:4:"ed68";s:14:"pi1/ce_wiz.gif";s:4:"cf99";s:38:"pi1/class.tx_jheadventcalender_pi1.php";s:4:"bbfc";s:46:"pi1/class.tx_jheadventcalender_pi1_wizicon.php";s:4:"8bee";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"4c0b";s:17:"pi1/locallang.xml";s:4:"0b81";s:16:"res/css/ajax.css";s:4:"c195";s:23:"res/img/ajax-loader.gif";s:4:"817b";s:21:"res/img/bg_dialog.gif";s:4:"2841";s:20:"res/img/bt_close.gif";s:4:"e595";s:33:"res/js/jquery/jquery-1.5.1.min.js";s:4:"5238";s:30:"res/js/jquery/jquery.cookie.js";s:4:"1252";s:26:"res/js/snow/jquery.snow.js";s:4:"ff42";s:30:"res/js/snow/jquery.snow.min.js";s:4:"4721";s:28:"res/js/snowstorm/license.txt";s:4:"ea96";s:33:"res/js/snowstorm/snowstorm-min.js";s:4:"244c";}',
 	'suggests' => array(
 	),
 );
